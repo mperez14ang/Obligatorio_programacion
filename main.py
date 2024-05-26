3cdf93cb3b7e751635e8112fb21c6c793906f45c
Manuel Pérez <146390467+mperez14ang@users.noreply.github.com>
2024-05-26 19:37:50 -0300

    Texto git

---
+++ main.py
@@ -0,0 +1,7 @@
+#2 ramas Edicion_1 y Edicion_2 para editarlas get checkout Edicion_1 o _2
+# Para cambiar de rama git checkout nombre y para crear una rama nueva git checkout -b Nombre
+# Para commit git add .
+# git commit -m Mensaje
+# git push origin 
+#Para mergear se hace desde github, sino con checkout merge creo q era asi 
+# Para actualizar una rama git checkout main, git pull origin main, git checkout Edicion_2, git merge
